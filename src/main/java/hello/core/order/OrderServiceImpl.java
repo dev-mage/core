c8b1432fd64df1af60f16e7e40db2568313b27bb
dev-mage <hxxlxx0@gmail.com>
2022-12-01 20:17:27 +0900

    섹션 2. 스프링 핵심 원리 이해1 - 예제 만들기(주문 도메인)

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        // 단일 책임 원칙을 잘 지켰기 때문에 할인에 문제가 생기면 할인만 수정하면 됨
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
